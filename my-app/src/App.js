6c58753201832019e88cae0f31e4740cc90f3fac
bigmoe49er <bigmoe49er@gmail.com>
2020-05-08 20:11:02 -0700

    concat numbers

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const [numberone, setnumberone]= useState(0); 
+  const [numbertwo, setnumbertwo] = useState(0);
+  const [result, setResult] = useState(0);
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        Calculator
+        <form>
+  <label>
+    Number 1:
+    <input type="number" value={numberone} onChange={e => setnumberone(e.target.value)} number="input 1" />
+  </label>
+  <label>
+    Number 2:
+    <input type="number" value={numbertwo} onChange={e => setnumbertwo(e.target.value)} number="input 2" />
+  </label>
+  <button onClick={() => {setResult(numberone + numbertwo)}}>
+    submit
+  </button>
+</form>
+<label>
+  Result: {result}  
+</label>
+  
     </div>
   );
 }
